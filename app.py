9b0e63d693f3707b5cbf654637f0917cb65364bb
joshcorby28 <josh@lab-13.co.uk>
2025-10-02 10:01:23 +0100

    Enable debug mode for local development

--- app.py
+++ app.py
@@ -229,4 +229,4 @@ def get_replacement_movie():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
